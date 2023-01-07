794618fdea7e07f9afb6d502bef85c3cf3f2158b
salazarSD <satyam9mzp@gmail.com>
2023-01-07 09:59:50 +0530

    insertion sort complete..

--- scripts/insertion.js
+++ scripts/insertion.js
@@ -6,38 +6,46 @@ we #INSERT elements one by one in sorted from unsorted on its right position
 so thaat whole arary tends to sorted. */
 //logic
 function insertion(){
+    c_delay=0;
     //let 1st element is sorted
-    //sorted part is shown by pink
-    div_update(divs[0],div_sizes[0],"pink");
     // div_update(divs[0],div_sizes[0],"pink");
     //travel to whole 
-    for(let i=1;i<array_size;i++){
+    for(var i=0;i<array_size;i++){
         //picking up current value which is to be inserted at right
+        div_update(divs[i],div_sizes[i],"yellow");//Color update
         var current=div_sizes[i];
         //chaging color while picking
-        div_update(divs[i],div_sizes[i],"white");
         
         //until the current is smaller push the values at 1 index higher
         let j=i-1;
         //pick j changed color
         while(j >=0 && current<div_sizes[j]){
-            div_update(divs[j],div_sizes[j],"yellow");
+            div_update(divs[j],div_sizes[j],"red");//Color update
+            div_update(divs[j+1],div_sizes[j+1],"red");//Color update
             //changing position of current element
             div_sizes[j+1]=div_sizes[j];
-            div_update(divs[j+1],div_sizes[j+1],"blue");
-            //I dont know logically (of insertion sort techinque )is going right or not but 
-            //its imp for visulizing
-            div_update(divs[j],current,"white"); 
-            //updating j and its color
-            j--;
+
+             div_update(divs[j],div_sizes[j],"red");//Height update
+            div_update(divs[j+1],div_sizes[j+1],"red");//Height update
+
+            div_update(divs[j],div_sizes[j],"blue");//Color update
+            if(j==(i-1))
+            {
+                div_update(divs[j+1],div_sizes[j+1],"yellow");//Color update
+            }
+            else
+            {
+                div_update(divs[j+1],div_sizes[j+1],"blue");//Color update
+            }
+            j-=1;
         }
+        //placement at right index(insretion)
+        div_sizes[j+1]=current;
         //color for sorted part
-        for(var k=0;k<j+1;k++){
-            div_update(divs[j],div_sizes[j],"pink");
+        for(var k=0;k<i;k++){
+            div_update(divs[k],div_sizes[k],"Green");
         }
-    //placement at right index(insretion)
-    div_sizes[j+1]=current;
-}
-// div_update(divs[j+1],div_sizes[j+1],"Green")
-    c_delay=0;
+    }
+    // div_update(divs[j+1],div_sizes[j+1],"Green")
+    div_update(divs[i-1],div_sizes[i-1],"green");//Color update
 }
